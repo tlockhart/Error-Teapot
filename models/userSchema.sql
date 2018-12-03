324cdcb277ddf697a167b60593f91b3d3253de06
Valverde Daniel <valverdedaniel44@gmail.com>
2018-12-03 13:16:32 -0500

    created usersSchema.sql

---
+++ models/userSchema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS avant_test_db;
+
+CREATE DATABASE avant_test_db;
+
+USE avant_test_db;
+
+/* CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(50) NULL,
+  email varchar(100) NULL,
+  password varchar(64) NULL,
+  PRIMARY KEY (id)
+); */
