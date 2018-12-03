a690ff688a4a57968a5103ce5c7bd7ff86958a37
tlockhart <tony.lockhart@ymail.com>
2018-12-03 18:57:12 -0500

    Added models

---
+++ models/artists.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, DataTypes) {
+  var Artist = sequelize.define("Artist", {
+    artistName: {
+      type: DataTypes.STRING,
+      unique: true, //can't have dups
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    bio: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    avatarUrl: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+  Artist.associate = function(models) {
+    // Artist and Artifacts Association: An Artist has many Artifacts
+    Artist.hasMany(
+      models.Artifact
+    ); /*, {
+          //onDelete: "CASCADE",// When an Artist is deleted, also delete any associated Artifacts
+          foreignKey: {
+            allowNull: true
+          }
+        });*/
+  };
+  return Artist;
+};
