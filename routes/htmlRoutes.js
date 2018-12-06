f7b10b73db5a2684ffdb43c40d7805d294391ed5
Valverde Daniel <valverdedaniel44@gmail.com>
2018-12-05 22:21:17 -0500

    re-added * in htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -39,8 +39,8 @@ module.exports = function(app) {
   /***************************************
    * THIS SHOULD BE THE LAST ROUTE
    ***************************************/
-  // Render 404 page for any unmatched routes
-  // app.get("*", function(req, res) {
-  //   res.render("404");
-  // });
+  Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
 };
