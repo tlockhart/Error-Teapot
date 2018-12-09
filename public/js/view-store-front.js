e81ef78ccdfd088888f378b75827a8ac366769db
tlockhart <tony.lockhart@ymail.com>
2018-12-09 17:50:00 -0500

    Added find-artist.js and updated add-listing.js, view-store-front.js, apiRoutes.js, and added ids to index.handlebars and main.handlebars.

--- public/js/view-store-front.js
+++ public/js/view-store-front.js
@@ -1,29 +1,31 @@
-$(function() {
-  var $viewStoreFrontBtn = $("#view-store-front-btn");
-  function viewStoreFront() {
-    //disable resubmit button
-    //$("#create-listing-btn").prop("disabled", true);
+$(document).ready(function() {
+  $(function() {
+    var $viewStoreFrontBtn = $("#view-store-front-btn");
+    function viewStoreFront() {
+      //disable resubmit button
+      //$("#create-listing-btn").prop("disabled", true);
 
-    clearPage();
-    var $dataId = $("#create-listing-btn").attr("data-id");
-    //REQUIRED: MUST USE TO REDIRECT to new view
-    location.replace("/display-store-front/" + $dataId);
-  } //display storefront
+      clearPage();
+      var $dataId = $("#create-listing-btn").attr("data-id");
+      //REQUIRED: MUST USE TO REDIRECT to new view
+      location.replace("/display-store-front/" + $dataId);
+    } //display storefront
 
-  function clearPage() {
-    $("#listing-title").val("");
-    $("#listing-price").val("");
-    $("#listing-thumb-img").val("");
-    $("#listing-full-img").val("");
-  }
+    function clearPage() {
+      $("#listing-title").val("");
+      $("#listing-price").val("");
+      $("#listing-thumb-img").val("");
+      $("#listing-full-img").val("");
+    }
 
-  $viewStoreFrontBtn.on("click", function(event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-    console.log("VIEW STORE FRONT BTN CLICKED");
-    /*********************/
-    //VIEW STOREFRONT:
-    /*********************/
-    viewStoreFront();
-  }); //ViewStoreFront Btn click
-}); //Event Listener functions
+    $viewStoreFrontBtn.on("click", function (event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+      console.log("VIEW STORE FRONT BTN CLICKED");
+      /*********************/
+      //VIEW STOREFRONT:
+      /*********************/
+      viewStoreFront();
+    }); //ViewStoreFront Btn click
+  }); //Event Listener functions
+}); //document on ready
